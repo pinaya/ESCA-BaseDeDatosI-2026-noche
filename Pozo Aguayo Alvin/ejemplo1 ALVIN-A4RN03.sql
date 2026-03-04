086518a1b7ad3da7c7491af9853592b8a302507c
pinaya <82481701+pinaya@users.noreply.github.com>
2026-03-04 15:07:36 -0400

    Add files via upload

---
+++ Pozo Aguayo Alvin/ejemplo1 ALVIN-A4RN03.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.5
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 03-03-2026 a las 00:30:52
+-- Versión del servidor: 5.5.16
+-- Versión de PHP: 5.3.8
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `ejemplo1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE IF NOT EXISTS `cliente` (
+  `id_cliente` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(40) NOT NULL,
+  PRIMARY KEY (`id_cliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `entrenador`
+--
+
+CREATE TABLE IF NOT EXISTS `entrenador` (
+  `id_entrenador` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(40) NOT NULL,
+  `especialidad` varchar(5) NOT NULL,
+  `telefono` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_entrenador`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rutina`
+--
+
+CREATE TABLE IF NOT EXISTS `rutina` (
+  `id_rutina` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_rutina` varchar(2) NOT NULL,
+  `duracion` varchar(50) NOT NULL,
+  `descripcion` varchar(20) NOT NULL,
+  `id_clinete1` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_rutina`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
